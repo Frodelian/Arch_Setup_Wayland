4ddad281c79d6d18a66cb4c1aad9cff141d25f0c
Frodelian <olegbodwk@gmail.com>
2025-06-28 00:38:22 +0300

    dwl19 and sls

--- startdwl.sh
+++ startdwl.sh
@@ -1,2 +1,2 @@
-export XKB_DEFAULT_OPTIONS="caps:swapescape"
-exec dwl -s 'dwlb'
+wlr-randr --output HDMI-A-1 --mode 1920x1080@144.013 --pos 0,0 --output HDMI-A-2 --mode 1920x1080@144.013 --pos 1920,0
+exec slstatus | dwl
